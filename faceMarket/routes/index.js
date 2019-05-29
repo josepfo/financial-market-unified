720460a28ea45a8724b230d7d6b0b957741c9f9f
Diana Costa <dianasofia.costa@sapo.pt>
2019-05-29 23:57:06 +0100

    front end sign in/up page done - 1st version

--- faceMarket/routes/index.js
+++ faceMarket/routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
 });
+
+/* Sign in or Sign up */
+router.get('/signinup', function(req, res, next) {
+	res.render('signinup');
+});
+
 
 module.exports = router;
