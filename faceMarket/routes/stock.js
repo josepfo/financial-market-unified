7294aa8417caf50bb16fe460ca567c53e240cf0c
José Pedro Oliveira <jpedrooliveira.w@gmail.com>
2019-06-04 19:51:55 +0100

    routes for stocks done

--- faceMarket/routes/stock.js
+++ faceMarket/routes/stock.js
@@ -1,9 +1,47 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
+
+API_KEY = '&apikey=STKRW67RG4VYBDMK'
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/:stock/:period', function(req, res) {
+  var time_period
+  var renderfile
+  if(req.params.period.localeCompare("hourly") == 0){
+     
+      axios.get('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol='+ req.params.stock +'&interval=60min&outputsize=compact' + API_KEY)
+      .then(resposta=> res.render('stockHourly', { answer: resposta.data }))
+      .catch(erro => {
+        console.log('Erro ao fazer pedido a stock.')
+        res.render('error', {error: erro, message: "Erro ao fazer pedido a Alphavantage."})
+      })
+      
+  }else{
+
+      if(req.params.period.localeCompare("daily") == 0){
+        time_period = "TIME_SERIES_DAILY"
+        renderfile = "stockDaily"
+      }else
+      if(req.params.period.localeCompare("weekly") == 0){
+        time_period = "TIME_SERIES_WEEKLY"
+        renderfile = "stockWeekly"
+      }else
+      if(req.params.period.localeCompare("monthly") == 0){
+        time_period = "TIME_SERIES_MONTHLY"
+        renderfile = "stockMonthly"
+      }
+    
+      axios.get('https://www.alphavantage.co/query?function=' + time_period + '&symbol=' + req.params.stock + API_KEY)
+        .then(resposta=> res.render(renderfile, { answer: resposta.data }))
+        .catch(erro => {
+          console.log('Erro ao fazer pedido a stock.')
+          res.render('error', {error: erro, message: "Erro ao fazer pedido a Alphavantage."})
+        })
+
+     
+  }
+  
 });
 
 module.exports = router;
