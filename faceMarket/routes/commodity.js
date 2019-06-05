7959154f75b3762caffefb83c966be9dc17159b4
Vitor Castro <v.j.r.c1997@hotmail.com>
2019-06-05 23:50:54 +0100

    joining axios responses

--- faceMarket/routes/commodity.js
+++ faceMarket/routes/commodity.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
+
+const API_KEY = '&api_key=QY8zng_hFHyM3DEwWxiM';
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+
+  axios.all([
+    // GOLD COMEX
+    axios.get('https://www.quandl.com/api/v3/datasets/CHRIS/CME_GC1/data.json?&limit=1&collapse=daily' + API_KEY)
+  ])
+  .then(axios.spread((resposta1, resposta2) => {
+    //console.log( JSON.stringify(resposta1.data.Data, null, 2) )
+    //console.log( JSON.stringify(resposta2.data.Data, null, 2) )
+
+    // concatenate both responses
+    var target = [].concat(resposta1.data.Data, resposta2.data.Data);
+
+    // pass only the Data field of the coins and values
+    console.log(target)
+    //res.render('commodity', { answer: target })
+  }))
+  .catch(erro => {
+    console.log('Erro ao fazer pedido a commodity.')
+    res.render('error', {error: erro, message: "Erro ao fazer pedido a Quandl."})
+  })
+
 });
 
 module.exports = router;
