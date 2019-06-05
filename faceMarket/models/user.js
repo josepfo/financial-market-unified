19868bd44b6277d5130472732de664308aa17d4f
czgcampos <carloszgcampos1996@gmail.com>
2019-06-06 00:58:53 +0100

    Fix models again

--- faceMarket/models/user.js
+++ faceMarket/models/user.js
@@ -3,7 +3,8 @@ var Schema = mongoose.Schema
 
 var UserSchema = new Schema({
 	email: {type: String, required: true},
-	password: {type: String, required: true}
+	password: {type: String, required: true},
+	descricao: {type: String}
 })
 
 module.exports = mongoose.model('Users', UserSchema, 'users')
