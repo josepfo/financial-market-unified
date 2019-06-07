4f0a9a321d1109f8cd158d32198c5f2a124383e1
Vitor Castro <v.j.r.c1997@hotmail.com>
2019-06-07 10:40:34 +0100

    Adding filters

--- faceMarket/routes/commodity.js
+++ faceMarket/routes/commodity.js
@@ -9,23 +9,102 @@ router.get('/', function(req, res) {
 
   axios.all([
     // GOLD COMEX
-    axios.get('https://www.quandl.com/api/v3/datasets/CHRIS/CME_GC1/data.json?&limit=1&collapse=daily' + API_KEY)
+    axios.get('https://www.quandl.com/api/v3/datasets/CHRIS/CME_GC1/data.json?&limit=1&collapse=daily' + API_KEY),
+    // SILVER COMEX
+    axios.get('https://www.quandl.com/api/v3/datasets/CHRIS/CME_SI1/data.json?&limit=1&collapse=daily' + API_KEY),
+    // PLATINUM NYMEX
+    axios.get('https://www.quandl.com/api/v3/datasets/CHRIS/CME_PL1/data.json?&limit=1&collapse=daily' + API_KEY),
+    // PALADIUM NYMEX
+    axios.get('https://www.quandl.com/api/v3/datasets/CHRIS/CME_PA1/data.json?&limit=1&collapse=daily' + API_KEY),
+    // COPPER COMEX
+    axios.get('https://www.quandl.com/api/v3/datasets/CHRIS/CME_HG1/data.json?&limit=1&collapse=daily' + API_KEY),
+    // OIL NYMEX
+    axios.get('https://www.quandl.com/api/v3/datasets/CHRIS/CME_CL1/data.json?&limit=1&collapse=daily' + API_KEY),
+    // NATURAL GAS NYMEX
+    axios.get('https://www.quandl.com/api/v3/datasets/CHRIS/CME_NG1/data.json?&limit=1&collapse=daily' + API_KEY)
   ])
-  .then(axios.spread((resposta1, resposta2) => {
-    //console.log( JSON.stringify(resposta1.data.Data, null, 2) )
-    //console.log( JSON.stringify(resposta2.data.Data, null, 2) )
+  .then(axios.spread((r1,r2,r3,r4,r5,r6,r7) => {
+    
+    // add commodity name
+    r1.data.dataset_data.data[0].unshift('Gold')
+    r2.data.dataset_data.data[0].unshift('Silver')
+    r3.data.dataset_data.data[0].unshift('Platinum')
+    r4.data.dataset_data.data[0].unshift('Paladium')
+    r5.data.dataset_data.data[0].unshift('Copper')
+    r6.data.dataset_data.data[0].unshift('Oil')
+    r7.data.dataset_data.data[0].unshift('Natural Gas')
 
     // concatenate both responses
-    var target = [].concat(resposta1.data.Data, resposta2.data.Data);
+    var target = [].concat(
+                            r1.data.dataset_data.data,
+                            r2.data.dataset_data.data,
+                            r3.data.dataset_data.data,
+                            r4.data.dataset_data.data,
+                            r5.data.dataset_data.data,
+                            r6.data.dataset_data.data,
+                            r7.data.dataset_data.data
+                          );
 
-    // pass only the Data field of the coins and values
-    console.log(target)
-    //res.render('commodity', { answer: target })
+    //console.log(target)
+    res.render('commodity', { answer: target })
   }))
   .catch(erro => {
     console.log('Erro ao fazer pedido a commodity.')
     res.render('error', {error: erro, message: "Erro ao fazer pedido a Quandl."})
-  })
+  });
+
+});
+
+/* GET Commodity Period. */
+router.get('/:com/:period', function(req, res) {
+  // get period
+  var time_period
+  if(req.params.period.localeCompare("hourly") == 0) {
+    time_period = 'none'
+  }
+  else if(req.params.period.localeCompare("daily") == 0) {
+    time_period = 'daily'
+  }
+  else if(req.params.period.localeCompare("weekly") == 0) {
+    time_period = 'weekly'
+  }
+  else if(req.params.period.localeCompare("monthly") == 0) {
+    time_period = 'monthly'
+  }
+
+  // get commodity
+  var com
+  if(req.params.com.localeCompare("Gold") == 0) {
+    com = 'CME_GC1'
+  }
+  else if(req.params.com.localeCompare("Silver") == 0) {
+    com = 'CME_SI1'
+  }
+  else if(req.params.com.localeCompare("Platinum") == 0) {
+    com = 'CME_PL1'
+  }
+  else if(req.params.com.localeCompare("Paladium") == 0) {
+    com = 'CME_PA1'
+  }
+  else if(req.params.com.localeCompare("Copper") == 0) {
+    com = 'CME_HG1'
+  }
+  else if(req.params.com.localeCompare("Oil") == 0) {
+    com = 'CME_CL1'
+  }
+  else if(req.params.com.localeCompare("Natural Gas") == 0) {
+    com = 'CME_NG1'
+  }
+
+  axios.get('https://www.quandl.com/api/v3/datasets/CHRIS/' + com + '/data.json?&limit=100&collapse=' + time_period + API_KEY)
+    .then(resposta=> {
+      //console.log(resposta.data.dataset_data.data)
+      res.render('commodity-time', { commodity: req.params.com + ' ' + req.params.period, answer: resposta.data.dataset_data.data })
+    })
+    .catch(erro => {
+      console.log('Erro ao fazer pedido a commodity.')
+      res.render('error', {error: erro, message: "Erro ao fazer pedido a Quandl."})
+    })
 
 });
 
--- faceMarket/routes/crypto.js
+++ faceMarket/routes/crypto.js
@@ -1,17 +1,56 @@
 var express = require('express');
 var router = express.Router();
 var axios = require('axios');
+const moment = require('moment');
 
 const API_KEY = '&api_key={031d68b9a5d59bce09ffa82ef258b56f97a125a7aebd7ba266e3583035ad3ed6}';
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  axios.get('https://min-api.cryptocompare.com/data/top/mktcapfull?limit=100&tsym=USD' + API_KEY)
+  axios.get('https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD' + API_KEY)
     .then(resposta=> res.render('crypto', { answer: resposta.data.Data }))
     .catch(erro => {
       console.log('Erro ao fazer pedido a crypto.')
       res.render('error', {error: erro, message: "Erro ao fazer pedido a Cryptocompare."})
     })
+});
+
+/* GET Crypto Period. */
+router.get('/:crypto/:period', function(req, res) {
+  // get period
+  var time_period
+  switch (req.params.period) {
+    case "hourly":
+      time_period = 'histohour'
+      break;
+    case "daily":
+      time_period = 'histoday'
+      break;
+    default:
+      console.log("Período escolhido indisponível ou errado.")
+      break;
+  }
+
+  axios.get('https://min-api.cryptocompare.com/data/' + time_period + '?fsym=' + req.params.crypto + '&tsym=USD&limit=10' + API_KEY)
+    .then(resposta=> {
+      
+      var ans = resposta.data.Data
+      //console(ans)
+      // reverter lista
+      //ans = ans.reverse()
+      // pegar apenas 10 primeiros elementos
+      //ans = ans.slice(0, 10)
+      
+      // modificar milisec para date
+      //ans.forEach(entry => entry.time = moment(entry.time).format('MMMM MM YYYY h:mm:ss'));
+      //console.log(resposta.data.dataset_data.data)
+      res.render('crypto-time', { crypto: req.params.crypto + ' ' + req.params.period, answer: ans })
+    })
+    .catch(erro => {
+      console.log('Erro ao fazer pedido a commodity.')
+      res.render('error', {error: erro, message: "Erro ao fazer pedido a Cryptocompare."})
+    })
+
 });
 
 module.exports = router;
