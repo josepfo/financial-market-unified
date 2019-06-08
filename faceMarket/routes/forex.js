b0cf0fc501f80f4ccfe6d364237567ee6fde50c2
José Pedro Oliveira <jpedrooliveira.w@gmail.com>
2019-06-08 20:17:31 +0100

    less .pugs and title update

--- faceMarket/routes/forex.js
+++ faceMarket/routes/forex.js
@@ -12,11 +12,11 @@ router.get('/', function(req, res, next) {
 /* GET Pair Period. */
 router.get('/:fst/:snd/:period', function(req, res) {
   var time_period
-  var renderfile
+  
   if(req.params.period.localeCompare("hourly") == 0){
       
       axios.get('https://www.alphavantage.co/query?function=FX_INTRADAY&from_symbol='+req.params.fst+'&to_symbol='+req.params.snd+'&interval=60min&outputsize=compact' + API_KEY)
-      .then(resposta=> res.render('forexHourly', { answer: resposta.data }))
+      .then(resposta=> res.render('forex', { answer: resposta.data }))
       .catch(erro => {
         console.log('Erro ao fazer pedido a forex.')
         res.render('error', {error: erro, message: "Erro ao fazer pedido a Alphavantage."})
@@ -27,20 +27,20 @@ router.get('/:fst/:snd/:period', function(req, res) {
 
       if(req.params.period.localeCompare("daily") == 0){
         time_period = "FX_DAILY"
-        renderfile = 'forexDaily'
+        
       }else
       if(req.params.period.localeCompare("weekly") == 0){
         time_period = "FX_WEEKLY"
-        renderfile = 'forexWeekly'
+        
       }else
       if(req.params.period.localeCompare("monthly") == 0){
         time_period = "FX_MONTHLY"
-        renderfile = 'forexMonthly'
+        
       }
     
       axios.get('https://www.alphavantage.co/query?function=' + time_period + '&from_symbol=' + req.params.fst + '&to_symbol=' + req.params.snd + API_KEY)
         .then(resposta=> {
-          res.render(renderfile, { answer: resposta.data })
+          res.render('forex', { answer: resposta.data })
           console.warn(resposta.data)
         })
         .catch(erro => {
--- faceMarket/routes/stock.js
+++ faceMarket/routes/stock.js
@@ -7,11 +7,11 @@ API_KEY = '&apikey=STKRW67RG4VYBDMK'
 /* GET home page. */
 router.get('/:stock/:period', function(req, res) {
   var time_period
-  var renderfile
+ 
   if(req.params.period.localeCompare("hourly") == 0){
      
       axios.get('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol='+ req.params.stock +'&interval=60min&outputsize=compact' + API_KEY)
-      .then(resposta=> res.render('stockHourly', { answer: resposta.data }))
+      .then(resposta=> res.render('stock', { answer: resposta.data }))
       .catch(erro => {
         console.log('Erro ao fazer pedido a stock.')
         res.render('error', {error: erro, message: "Erro ao fazer pedido a Alphavantage."})
@@ -21,19 +21,19 @@ router.get('/:stock/:period', function(req, res) {
 
       if(req.params.period.localeCompare("daily") == 0){
         time_period = "TIME_SERIES_DAILY"
-        renderfile = "stockDaily"
+       
       }else
       if(req.params.period.localeCompare("weekly") == 0){
         time_period = "TIME_SERIES_WEEKLY"
-        renderfile = "stockWeekly"
+       
       }else
       if(req.params.period.localeCompare("monthly") == 0){
         time_period = "TIME_SERIES_MONTHLY"
-        renderfile = "stockMonthly"
+       
       }
     
       axios.get('https://www.alphavantage.co/query?function=' + time_period + '&symbol=' + req.params.stock + API_KEY)
-        .then(resposta=> res.render(renderfile, { answer: resposta.data }))
+        .then(resposta=> res.render('stock', { answer: resposta.data }))
         .catch(erro => {
           console.log('Erro ao fazer pedido a stock.')
           res.render('error', {error: erro, message: "Erro ao fazer pedido a Alphavantage."})
